68a768795992f8d597627850c123f7cc6c8d1bc3
Roman-K-S <70799864+Roman-K-S@users.noreply.github.com>
2022-04-14 21:27:34 +0300

    Lesson 4

---
+++ src/main/java/ru/gb/springlesson4/service/ProductService.java
@@ -0,0 +1,36 @@
+package ru.gb.springlesson4.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.gb.springlesson4.model.Product;
+import ru.gb.springlesson4.repository.ProductRepository;
+
+import java.util.List;
+
+    @Service
+    @RequiredArgsConstructor
+    public class ProductService {
+
+        private final ProductRepository productRepository;
+
+        public Product save(Product product) {
+            return productRepository.save(product);
+        }
+
+        public Product edit(Product product){
+            return productRepository.edit(product);
+        }
+
+        public Product viewById(Integer id){
+            return productRepository.viewById(id).orElse(new Product());
+        }
+
+        public List<Product> viewAll() {
+            return productRepository.viewAll();
+        }
+
+        public void deleteById(Integer id){
+            productRepository.deleteById(id);
+        }
+
+    }
